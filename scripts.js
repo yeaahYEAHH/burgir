53a674ad7b0348274e9debdc970afc6ffaaf1d0d
yeaahYEAHH <81806907+yeaahYEAHH@users.noreply.github.com>
2023-01-30 10:02:27 +0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.menu-burger__header').click(function() {
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.header__nav').toggleClass('open-menu');
+    });
+});
